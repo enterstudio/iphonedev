163bc80b361b53f21580e8d2f7be9a90ed1134ca
miketihonchik <mike.tihonchik@gmail.com>
2013-11-01 11:39:31 -0500

    moving character!!!! yey....

--- GameWithNoName/GameWithNoName/MyScene.m
+++ GameWithNoName/GameWithNoName/MyScene.m
@@ -6,63 +6,56 @@
 //  Copyright (c) 2013 mike.tihonchik. All rights reserved.
 //
 
+#import <AVFoundation/AVFoundation.h>
 #import "MyScene.h"
 
-@interface MyScene ()
+@implementation MyScene {
+    
+    SKSpriteNode *_player;
+    NSArray *_movementFrames;
+}
 
-@property (nonatomic, strong) CALayer *layer;
-
-@end
-
-@implementation MyScene
-
--(id)initWithSize:(CGSize)size {    
+-(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
+        self.backgroundColor = [SKColor blackColor];
+
+        NSMutableArray *moveFrames = [NSMutableArray array];
+        SKTextureAtlas *playerAtlas = [SKTextureAtlas atlasNamed:@"nonamegame"];
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        int numImages = playerAtlas.textureNames.count;
+        /* divide number of frames by 2, if there are iPad sized images - Mike T., 11/1/2013 */
+        for (int i=1; i <= numImages; i++) {
+            NSString *textureName;
+            if(i<10) {
+                textureName = [NSString stringWithFormat:@"slice0%d_0%d", i, i];
+            } else {
+                textureName = [NSString stringWithFormat:@"slice%d_%d", i, i];
+            }
+            SKTexture *temp = [playerAtlas textureNamed:textureName];
+            [moveFrames addObject:temp];
+        }
+        _movementFrames = moveFrames;
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
-        [self loadSpriteSheet];
+        SKTexture *temp = _movementFrames[0];
+        _player = [SKSpriteNode spriteNodeWithTexture:temp];
+        _player.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+        [self addChild:_player];
+        [self movingCharacter];
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-
-}
-
 -(void)update:(CFTimeInterval)currentTime {
+
 }
 
--(void)loadSpriteSheet {
-    // "sprites.png" is a 192x128 picture containing two rows of three 64x64 sprites each
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"spy_sheet.png" ofType:nil];
-    CGImageRef img = [UIImage imageWithContentsOfFile:path].CGImage;
-    CALayer *imgLayer = [CALayer layer];
-    imgLayer.contents = (__bridge id)img;
-    // Frame defines the position of your sprite inside your view
-    imgLayer.frame = CGRectMake(0, 0, 64, 64);
-    imgLayer.contentsRect = CGRectMake(0, 0, 64, 64); // The size of an individual sprite
-    // Pick the sprite from the top row, left column
-    imgLayer.contentsRect = CGRectMake(0, 0, 1.0/3.0, 1.0/2.0);
-    // Top row, middle column
-    // imgLayer.contentsRect = CGRectMake(1.0/3, 0, 1.0/3.0, 1.0/2.0);
-    // Top row, right column
-    // imgLayer.contentsRect = CGRectMake(2.0/3, 0, 1.0/3.0, 1.0/2.0);
-    // Bottom row, left column
-    // imgLayer.contentsRect = CGRectMake(0, 1.0/2, 1.0/3.0, 1.0/2.0);
-    // Bottom row, middle column
-    // imgLayer.contentsRect = CGRectMake(1.0/3, 1.0/2, 1.0/3.0, 1.0/2.0);
-    // Bottom row, right column
-    // imgLayer.contentsRect = CGRectMake(2.0/3, 1.0/2, 1.0/3.0, 1.0/2.0);
-    [self.layer addSublayer:imgLayer];
+-(void)movingCharacter {
+    // This is our general runAction method to make our bear walk.
+    [_player runAction:[SKAction repeatActionForever:
+                      [SKAction animateWithTextures:_movementFrames
+                                       timePerFrame:0.3f
+                                             resize:NO
+                                            restore:YES]] withKey:@"inplaceMovement"];
+    return;
 }
 @end
---
+++ GameWithNoName/GameWithNoName/spritekitimages/nonamegame.h
@@ -0,0 +1,124 @@
+// ---------------------------------------
+// Sprite definitions for nonamegame
+// Generated with TexturePacker 3.1.3
+//
+// http://www.codeandweb.com/texturepacker
+// ---------------------------------------
+
+#ifndef __NONAMEGAME_ATLAS__
+#define __NONAMEGAME_ATLAS__
+
+// ------------------------
+// name of the atlas bundle
+// ------------------------
+#define NONAMEGAME_ATLAS_NAME @"nonamegame"
+
+// ------------
+// sprite names
+// ------------
+#define NONAMEGAME_SPR_SLICE01_01 @"slice01_01"
+#define NONAMEGAME_SPR_SLICE02_02 @"slice02_02"
+#define NONAMEGAME_SPR_SLICE03_03 @"slice03_03"
+#define NONAMEGAME_SPR_SLICE04_04 @"slice04_04"
+#define NONAMEGAME_SPR_SLICE05_05 @"slice05_05"
+#define NONAMEGAME_SPR_SLICE06_06 @"slice06_06"
+#define NONAMEGAME_SPR_SLICE07_07 @"slice07_07"
+#define NONAMEGAME_SPR_SLICE08_08 @"slice08_08"
+#define NONAMEGAME_SPR_SLICE09_09 @"slice09_09"
+#define NONAMEGAME_SPR_SLICE10_10 @"slice10_10"
+#define NONAMEGAME_SPR_SLICE11_11 @"slice11_11"
+#define NONAMEGAME_SPR_SLICE12_12 @"slice12_12"
+#define NONAMEGAME_SPR_SLICE13_13 @"slice13_13"
+#define NONAMEGAME_SPR_SLICE14_14 @"slice14_14"
+#define NONAMEGAME_SPR_SLICE15_15 @"slice15_15"
+#define NONAMEGAME_SPR_SLICE16_16 @"slice16_16"
+
+// --------
+// textures
+// --------
+#define NONAMEGAME_TEX_SLICE01_01 [SKTexture textureWithImageNamed:@"slice01_01"]
+#define NONAMEGAME_TEX_SLICE02_02 [SKTexture textureWithImageNamed:@"slice02_02"]
+#define NONAMEGAME_TEX_SLICE03_03 [SKTexture textureWithImageNamed:@"slice03_03"]
+#define NONAMEGAME_TEX_SLICE04_04 [SKTexture textureWithImageNamed:@"slice04_04"]
+#define NONAMEGAME_TEX_SLICE05_05 [SKTexture textureWithImageNamed:@"slice05_05"]
+#define NONAMEGAME_TEX_SLICE06_06 [SKTexture textureWithImageNamed:@"slice06_06"]
+#define NONAMEGAME_TEX_SLICE07_07 [SKTexture textureWithImageNamed:@"slice07_07"]
+#define NONAMEGAME_TEX_SLICE08_08 [SKTexture textureWithImageNamed:@"slice08_08"]
+#define NONAMEGAME_TEX_SLICE09_09 [SKTexture textureWithImageNamed:@"slice09_09"]
+#define NONAMEGAME_TEX_SLICE10_10 [SKTexture textureWithImageNamed:@"slice10_10"]
+#define NONAMEGAME_TEX_SLICE11_11 [SKTexture textureWithImageNamed:@"slice11_11"]
+#define NONAMEGAME_TEX_SLICE12_12 [SKTexture textureWithImageNamed:@"slice12_12"]
+#define NONAMEGAME_TEX_SLICE13_13 [SKTexture textureWithImageNamed:@"slice13_13"]
+#define NONAMEGAME_TEX_SLICE14_14 [SKTexture textureWithImageNamed:@"slice14_14"]
+#define NONAMEGAME_TEX_SLICE15_15 [SKTexture textureWithImageNamed:@"slice15_15"]
+#define NONAMEGAME_TEX_SLICE16_16 [SKTexture textureWithImageNamed:@"slice16_16"]
+
+// ----------
+// animations
+// ----------
+#define NONAMEGAME_ANIM_SLICE01 @[ \
+        [SKTexture textureWithImageNamed:@"slice01_01"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE02 @[ \
+        [SKTexture textureWithImageNamed:@"slice02_02"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE03 @[ \
+        [SKTexture textureWithImageNamed:@"slice03_03"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE04 @[ \
+        [SKTexture textureWithImageNamed:@"slice04_04"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE05 @[ \
+        [SKTexture textureWithImageNamed:@"slice05_05"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE06 @[ \
+        [SKTexture textureWithImageNamed:@"slice06_06"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE07 @[ \
+        [SKTexture textureWithImageNamed:@"slice07_07"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE08 @[ \
+        [SKTexture textureWithImageNamed:@"slice08_08"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE09 @[ \
+        [SKTexture textureWithImageNamed:@"slice09_09"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE10 @[ \
+        [SKTexture textureWithImageNamed:@"slice10_10"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE11 @[ \
+        [SKTexture textureWithImageNamed:@"slice11_11"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE12 @[ \
+        [SKTexture textureWithImageNamed:@"slice12_12"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE13 @[ \
+        [SKTexture textureWithImageNamed:@"slice13_13"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE14 @[ \
+        [SKTexture textureWithImageNamed:@"slice14_14"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE15 @[ \
+        [SKTexture textureWithImageNamed:@"slice15_15"]  \
+    ]
+
+#define NONAMEGAME_ANIM_SLICE16 @[ \
+        [SKTexture textureWithImageNamed:@"slice16_16"]  \
+    ]
+
+
+#endif // __NONAMEGAME_ATLAS__
