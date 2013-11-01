f54a4ffbf1b82f369b0438957fa55fc5f2e034b5
miketihonchik <mike.tihonchik@gmail.com>
2013-11-01 15:57:59 -0500

    moving character around the screen

--- GameWithNoName/GameWithNoName/MyScene.m
+++ GameWithNoName/GameWithNoName/MyScene.m
@@ -12,35 +12,52 @@
 @implementation MyScene {
     
     SKSpriteNode *_player;
-    NSArray *_movementFrames;
+    NSArray *_movementFramesLeft;
+    NSArray *_movementFramesRight;
+    NSArray *_movementFramesUp;
+    NSArray *_movementFramesDown;
 }
 
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         self.backgroundColor = [SKColor blackColor];
 
-        NSMutableArray *moveFrames = [NSMutableArray array];
+        NSMutableArray *moveFramesLeft = [NSMutableArray array];
+        NSMutableArray *moveFramesRight = [NSMutableArray array];
+        NSMutableArray *moveFramesUp = [NSMutableArray array];
+        NSMutableArray *moveFramesDown = [NSMutableArray array];
         SKTextureAtlas *playerAtlas = [SKTextureAtlas atlasNamed:@"nonamegame"];
         
-        int numImages = playerAtlas.textureNames.count;
+        //int numImages = playerAtlas.textureNames.count;
         /* divide number of frames by 2, if there are iPad sized images - Mike T., 11/1/2013 */
-        for (int i=1; i <= numImages; i++) {
-            NSString *textureName;
-            if(i<10) {
-                textureName = [NSString stringWithFormat:@"slice0%d_0%d", i, i];
-            } else {
-                textureName = [NSString stringWithFormat:@"slice%d_%d", i, i];
-            }
+        for (int i=1; i <= 4; i++) {
+            
+            NSString *textureName = [NSString stringWithFormat:@"walk_up_%d", i];
             SKTexture *temp = [playerAtlas textureNamed:textureName];
-            [moveFrames addObject:temp];
+            [moveFramesUp addObject:temp];
+
+            textureName = [NSString stringWithFormat:@"walk_down_%d", i];
+            temp = [playerAtlas textureNamed:textureName];
+            [moveFramesDown addObject:temp];
+
+            textureName = [NSString stringWithFormat:@"walk_left_%d", i];
+            temp = [playerAtlas textureNamed:textureName];
+            [moveFramesLeft addObject:temp];
+            
+            textureName = [NSString stringWithFormat:@"walk_right_%d", i];
+            temp = [playerAtlas textureNamed:textureName];
+            [moveFramesRight addObject:temp];
         }
-        _movementFrames = moveFrames;
+        _movementFramesLeft = moveFramesLeft;
+        _movementFramesRight = moveFramesRight;
+        _movementFramesUp = moveFramesUp;
+        _movementFramesDown = moveFramesDown;
         
-        SKTexture *temp = _movementFrames[0];
+        /* Setting initial position */
+        SKTexture *temp = _movementFramesDown[0];
         _player = [SKSpriteNode spriteNodeWithTexture:temp];
         _player.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
         [self addChild:_player];
-        [self movingCharacter];
     }
     return self;
 }
@@ -49,13 +66,69 @@
 
 }
 
--(void)movingCharacter {
-    // This is our general runAction method to make our bear walk.
-    [_player runAction:[SKAction repeatActionForever:
-                      [SKAction animateWithTextures:_movementFrames
-                                       timePerFrame:0.3f
-                                             resize:NO
-                                            restore:YES]] withKey:@"inplaceMovement"];
+-(void)movingCharacter:(NSString *)move {
+    if([@"up" isEqualToString:move]) {
+        [_player runAction:[SKAction animateWithTextures:_movementFramesUp
+                                            timePerFrame:0.3f
+                                                  resize:NO
+                                                 restore:YES] withKey:@"movingUp"];
+    } else if([@"down" isEqualToString:move]) {
+        [_player runAction:[SKAction animateWithTextures:_movementFramesDown
+                                            timePerFrame:0.3f
+                                                  resize:NO
+                                                 restore:YES] withKey:@"movingDown"];
+    } else if([@"left" isEqualToString:move]) {
+        [_player runAction:[SKAction repeatActionForever:
+                            [SKAction animateWithTextures:_movementFramesLeft
+                                             timePerFrame:0.3f
+                                                   resize:NO
+                                                  restore:YES]] withKey:@"movingLeft"];
+    } else if([@"right" isEqualToString:move]) {
+        [_player runAction:[SKAction repeatActionForever:
+                            [SKAction animateWithTextures:_movementFramesRight
+                                             timePerFrame:0.3f
+                                                   resize:NO
+                                                  restore:YES]] withKey:@"movingRight"];
+    }
     return;
 }
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint location = [[touches anyObject] locationInNode:self];
+    CGSize screenSize = self.frame.size;
+    float playerVelocity = screenSize.width / 5.0;
+    CGPoint moveDifference = CGPointMake(location.x - _player.position.x, location.y - _player.position.y);
+    float distanceToMove = sqrtf(moveDifference.x * moveDifference.x + moveDifference.y * moveDifference.y);
+    float moveDuration = distanceToMove / playerVelocity;
+
+    if ([_player actionForKey:@"characterMoving"]) {
+        [_player removeActionForKey:@"characterMoving"];
+    }
+
+    if (moveDifference.x < 0) {
+        if (![_player actionForKey:@"movingLeft"]) {
+            [self movingCharacter:@"left"];
+        }
+    } else {
+        if (![_player actionForKey:@"movingRight"]) {
+            [self movingCharacter:@"right"];
+        }
+    }
+
+    SKAction *moveAction = [SKAction moveTo:location duration:moveDuration];
+    SKAction *doneAction = [SKAction runBlock:(dispatch_block_t)^() {
+        [self playerMoveEnded];
+    }];
+    SKAction *moveActionWithDone = [SKAction sequence:@[moveAction,doneAction]];
+    
+    [_player runAction:moveActionWithDone withKey:@"characterMoving"];
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+}
+
+-(void)playerMoveEnded {
+    [_player removeAllActions];
+}
+
 @end
--- GameWithNoName/GameWithNoName/spritekitimages/nonamegame.h
+++ GameWithNoName/GameWithNoName/spritekitimages/nonamegame.h
@@ -16,108 +16,72 @@
 // ------------
 // sprite names
 // ------------
-#define NONAMEGAME_SPR_SLICE01_01 @"slice01_01"
-#define NONAMEGAME_SPR_SLICE02_02 @"slice02_02"
-#define NONAMEGAME_SPR_SLICE03_03 @"slice03_03"
-#define NONAMEGAME_SPR_SLICE04_04 @"slice04_04"
-#define NONAMEGAME_SPR_SLICE05_05 @"slice05_05"
-#define NONAMEGAME_SPR_SLICE06_06 @"slice06_06"
-#define NONAMEGAME_SPR_SLICE07_07 @"slice07_07"
-#define NONAMEGAME_SPR_SLICE08_08 @"slice08_08"
-#define NONAMEGAME_SPR_SLICE09_09 @"slice09_09"
-#define NONAMEGAME_SPR_SLICE10_10 @"slice10_10"
-#define NONAMEGAME_SPR_SLICE11_11 @"slice11_11"
-#define NONAMEGAME_SPR_SLICE12_12 @"slice12_12"
-#define NONAMEGAME_SPR_SLICE13_13 @"slice13_13"
-#define NONAMEGAME_SPR_SLICE14_14 @"slice14_14"
-#define NONAMEGAME_SPR_SLICE15_15 @"slice15_15"
-#define NONAMEGAME_SPR_SLICE16_16 @"slice16_16"
+#define NONAMEGAME_SPR_WALK_DOWN_1  @"walk_down_1"
+#define NONAMEGAME_SPR_WALK_DOWN_2  @"walk_down_2"
+#define NONAMEGAME_SPR_WALK_DOWN_3  @"walk_down_3"
+#define NONAMEGAME_SPR_WALK_DOWN_4  @"walk_down_4"
+#define NONAMEGAME_SPR_WALK_LEFT_1  @"walk_left_1"
+#define NONAMEGAME_SPR_WALK_LEFT_2  @"walk_left_2"
+#define NONAMEGAME_SPR_WALK_LEFT_3  @"walk_left_3"
+#define NONAMEGAME_SPR_WALK_LEFT_4  @"walk_left_4"
+#define NONAMEGAME_SPR_WALK_RIGHT_1 @"walk_right_1"
+#define NONAMEGAME_SPR_WALK_RIGHT_2 @"walk_right_2"
+#define NONAMEGAME_SPR_WALK_RIGHT_3 @"walk_right_3"
+#define NONAMEGAME_SPR_WALK_RIGHT_4 @"walk_right_4"
+#define NONAMEGAME_SPR_WALK_UP_1    @"walk_up_1"
+#define NONAMEGAME_SPR_WALK_UP_2    @"walk_up_2"
+#define NONAMEGAME_SPR_WALK_UP_3    @"walk_up_3"
+#define NONAMEGAME_SPR_WALK_UP_4    @"walk_up_4"
 
 // --------
 // textures
 // --------
-#define NONAMEGAME_TEX_SLICE01_01 [SKTexture textureWithImageNamed:@"slice01_01"]
-#define NONAMEGAME_TEX_SLICE02_02 [SKTexture textureWithImageNamed:@"slice02_02"]
-#define NONAMEGAME_TEX_SLICE03_03 [SKTexture textureWithImageNamed:@"slice03_03"]
-#define NONAMEGAME_TEX_SLICE04_04 [SKTexture textureWithImageNamed:@"slice04_04"]
-#define NONAMEGAME_TEX_SLICE05_05 [SKTexture textureWithImageNamed:@"slice05_05"]
-#define NONAMEGAME_TEX_SLICE06_06 [SKTexture textureWithImageNamed:@"slice06_06"]
-#define NONAMEGAME_TEX_SLICE07_07 [SKTexture textureWithImageNamed:@"slice07_07"]
-#define NONAMEGAME_TEX_SLICE08_08 [SKTexture textureWithImageNamed:@"slice08_08"]
-#define NONAMEGAME_TEX_SLICE09_09 [SKTexture textureWithImageNamed:@"slice09_09"]
-#define NONAMEGAME_TEX_SLICE10_10 [SKTexture textureWithImageNamed:@"slice10_10"]
-#define NONAMEGAME_TEX_SLICE11_11 [SKTexture textureWithImageNamed:@"slice11_11"]
-#define NONAMEGAME_TEX_SLICE12_12 [SKTexture textureWithImageNamed:@"slice12_12"]
-#define NONAMEGAME_TEX_SLICE13_13 [SKTexture textureWithImageNamed:@"slice13_13"]
-#define NONAMEGAME_TEX_SLICE14_14 [SKTexture textureWithImageNamed:@"slice14_14"]
-#define NONAMEGAME_TEX_SLICE15_15 [SKTexture textureWithImageNamed:@"slice15_15"]
-#define NONAMEGAME_TEX_SLICE16_16 [SKTexture textureWithImageNamed:@"slice16_16"]
+#define NONAMEGAME_TEX_WALK_DOWN_1  [SKTexture textureWithImageNamed:@"walk_down_1"]
+#define NONAMEGAME_TEX_WALK_DOWN_2  [SKTexture textureWithImageNamed:@"walk_down_2"]
+#define NONAMEGAME_TEX_WALK_DOWN_3  [SKTexture textureWithImageNamed:@"walk_down_3"]
+#define NONAMEGAME_TEX_WALK_DOWN_4  [SKTexture textureWithImageNamed:@"walk_down_4"]
+#define NONAMEGAME_TEX_WALK_LEFT_1  [SKTexture textureWithImageNamed:@"walk_left_1"]
+#define NONAMEGAME_TEX_WALK_LEFT_2  [SKTexture textureWithImageNamed:@"walk_left_2"]
+#define NONAMEGAME_TEX_WALK_LEFT_3  [SKTexture textureWithImageNamed:@"walk_left_3"]
+#define NONAMEGAME_TEX_WALK_LEFT_4  [SKTexture textureWithImageNamed:@"walk_left_4"]
+#define NONAMEGAME_TEX_WALK_RIGHT_1 [SKTexture textureWithImageNamed:@"walk_right_1"]
+#define NONAMEGAME_TEX_WALK_RIGHT_2 [SKTexture textureWithImageNamed:@"walk_right_2"]
+#define NONAMEGAME_TEX_WALK_RIGHT_3 [SKTexture textureWithImageNamed:@"walk_right_3"]
+#define NONAMEGAME_TEX_WALK_RIGHT_4 [SKTexture textureWithImageNamed:@"walk_right_4"]
+#define NONAMEGAME_TEX_WALK_UP_1    [SKTexture textureWithImageNamed:@"walk_up_1"]
+#define NONAMEGAME_TEX_WALK_UP_2    [SKTexture textureWithImageNamed:@"walk_up_2"]
+#define NONAMEGAME_TEX_WALK_UP_3    [SKTexture textureWithImageNamed:@"walk_up_3"]
+#define NONAMEGAME_TEX_WALK_UP_4    [SKTexture textureWithImageNamed:@"walk_up_4"]
 
 // ----------
 // animations
 // ----------
-#define NONAMEGAME_ANIM_SLICE01 @[ \
-        [SKTexture textureWithImageNamed:@"slice01_01"]  \
+#define NONAMEGAME_ANIM_WALK_DOWN @[ \
+        [SKTexture textureWithImageNamed:@"walk_down_1"], \
+        [SKTexture textureWithImageNamed:@"walk_down_2"], \
+        [SKTexture textureWithImageNamed:@"walk_down_3"], \
+        [SKTexture textureWithImageNamed:@"walk_down_4"]  \
     ]
 
-#define NONAMEGAME_ANIM_SLICE02 @[ \
-        [SKTexture textureWithImageNamed:@"slice02_02"]  \
+#define NONAMEGAME_ANIM_WALK_LEFT @[ \
+        [SKTexture textureWithImageNamed:@"walk_left_1"], \
+        [SKTexture textureWithImageNamed:@"walk_left_2"], \
+        [SKTexture textureWithImageNamed:@"walk_left_3"], \
+        [SKTexture textureWithImageNamed:@"walk_left_4"]  \
     ]
 
-#define NONAMEGAME_ANIM_SLICE03 @[ \
-        [SKTexture textureWithImageNamed:@"slice03_03"]  \
+#define NONAMEGAME_ANIM_WALK_RIGHT @[ \
+        [SKTexture textureWithImageNamed:@"walk_right_1"], \
+        [SKTexture textureWithImageNamed:@"walk_right_2"], \
+        [SKTexture textureWithImageNamed:@"walk_right_3"], \
+        [SKTexture textureWithImageNamed:@"walk_right_4"]  \
     ]
 
-#define NONAMEGAME_ANIM_SLICE04 @[ \
-        [SKTexture textureWithImageNamed:@"slice04_04"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE05 @[ \
-        [SKTexture textureWithImageNamed:@"slice05_05"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE06 @[ \
-        [SKTexture textureWithImageNamed:@"slice06_06"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE07 @[ \
-        [SKTexture textureWithImageNamed:@"slice07_07"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE08 @[ \
-        [SKTexture textureWithImageNamed:@"slice08_08"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE09 @[ \
-        [SKTexture textureWithImageNamed:@"slice09_09"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE10 @[ \
-        [SKTexture textureWithImageNamed:@"slice10_10"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE11 @[ \
-        [SKTexture textureWithImageNamed:@"slice11_11"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE12 @[ \
-        [SKTexture textureWithImageNamed:@"slice12_12"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE13 @[ \
-        [SKTexture textureWithImageNamed:@"slice13_13"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE14 @[ \
-        [SKTexture textureWithImageNamed:@"slice14_14"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE15 @[ \
-        [SKTexture textureWithImageNamed:@"slice15_15"]  \
-    ]
-
-#define NONAMEGAME_ANIM_SLICE16 @[ \
-        [SKTexture textureWithImageNamed:@"slice16_16"]  \
+#define NONAMEGAME_ANIM_WALK_UP @[ \
+        [SKTexture textureWithImageNamed:@"walk_up_1"], \
+        [SKTexture textureWithImageNamed:@"walk_up_2"], \
+        [SKTexture textureWithImageNamed:@"walk_up_3"], \
+        [SKTexture textureWithImageNamed:@"walk_up_4"]  \
     ]
 
 
