e15eeff55c59715f8361978df59bd4a6bb19e7fa
miketihonchik <mike.tihonchik@gmail.com>
2014-03-31 15:01:26 -0500

    add required libraries; basic app layout

--- CardIODemo/CardIODemo/ViewController.m
+++ CardIODemo/CardIODemo/ViewController.m
@@ -7,23 +7,46 @@
 //
 
 #import "ViewController.h"
+#import "CardIO.h"
 
-@interface ViewController ()
+@interface ViewController () <CardIOPaymentViewControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *infoLabel;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+#pragma mark - View Lifecycle
+#pragma mark -
+
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.infoLabel.text = @"";
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - User Actions
+
+- (void)scanCardClicked:(id)sender {
+    CardIOPaymentViewController *scanViewController = [[CardIOPaymentViewController alloc] initWithPaymentDelegate:self];
+    scanViewController.modalPresentationStyle = UIModalPresentationFormSheet;
+    scanViewController.appToken = @"ec637aaed7e4446da73c385fbbf67aab";
+    [self presentViewController:scanViewController animated:YES completion:nil];
+}
+
+#pragma mark - CardIOPaymentViewControllerDelegate
+
+- (void)userDidProvideCreditCardInfo:(CardIOCreditCardInfo *)info inPaymentViewController:(CardIOPaymentViewController *)paymentViewController {
+    NSLog(@"Scan succeeded with info: %@", info);
+    // Do whatever needs to be done to deliver the purchased items.
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    self.infoLabel.text = [NSString stringWithFormat:@"Received card info. Number: %@, expiry: %02lu/%lu, cvv: %@.", info.redactedCardNumber, (unsigned long)info.expiryMonth, (unsigned long)info.expiryYear, info.cvv];
+}
+
+- (void)userDidCancelPaymentViewController:(CardIOPaymentViewController *)paymentViewController {
+    NSLog(@"User cancelled scan");
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
